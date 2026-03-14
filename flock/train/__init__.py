46f920e9176db5429d06c3b52af203c6441ceb04
Erik Helmers <erik.helmers@outlook.fr>
2026-03-14 16:51:19 +0100

    Remove shitty NN train poc
    Vibecoding without domain knowledge is hard. Should not have done that.

--- flock/train/__init__.py
+++
@@ -1,3 +0,0 @@
-from flock.train.policy import MLPPolicy, make_policy, obs_dim
-from flock.train.ppo import TrainConfig, Rollout, collect_rollouts, compute_gae, ppo_loss, train_step
-from flock.train.loop import train
